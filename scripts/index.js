0b01c4f885a73112f7f011b8c275bfecb5818d1a
sandip wadkar <sandip.wadkar@merkle.com>
2026-08-11 16:16:42 +0530

    fixed eslint

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,1 @@
-export * from './events.js'; 
-export * from './datalayer.js'; 
-export * from './payloadBuilder.js'; 
-export * from './metadata.js'; 
-export * from './observers.js'; 
-export * from './linktracking.js';
-export * from './pageview.js';
-export * from './utils.js';
+export { default as EVENTS } from './events.js'; 
