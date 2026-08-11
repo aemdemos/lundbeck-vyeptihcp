486d816a6e2fd421e46231b1e473272eeff358f9
sandip wadkar <sandip.wadkar@merkle.com>
2026-08-11 17:54:19 +0530

    fixed eslint

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,7 @@
 // add delayed functionality here
 import { loadScript } from './aem.js';
 import { pushPageViewEvent } from './pageview.js';
-import { initLinkTracking } from './linktracking.js';
+import initLinkTracking from './linktracking.js';
 import { initScrollDepthTracking } from './observers.js';
 
 // CookieInformation consent management platform (same CMP as www.vyepti.com)
--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
 export { default as EVENTS } from './events.js'; 
+
+export * from './datalayer.js';
+export * from './payloadBuilder.js';
+export * from './metadata.js';
+export * from './observers.js';
+export { default as initLinkTracking } from './linktracking.js';
+export * from './pageview.js';
+export * from './utils.js';
